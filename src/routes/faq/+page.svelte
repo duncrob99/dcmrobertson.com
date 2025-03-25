a86d50ed171fdb95001ddbddcf4eb5d4b295c5b7
duncrob99 <duncrob99@gmail.com>
2025-03-26 10:59:15 +1100

    Update copy (#17)
    * Add Cloudflare Turnstile to contact page
    
    * Update copy
    
    Add trial lesson FAQ and clarify subject offerings
    
    ---------
    
    Co-authored-by: Duncan Robertson <duncan@dcmrobertson.com>

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -15,6 +15,13 @@
 			I generally do one hour lessons, with a charge of $50/hr.
 		</p>
 	</Accordion>
+
+	<Accordion startOpen summary="Do you offer a trial lesson?">
+		<p>
+			Yes, your first lesson is free so we can get to know each other and make sure that I can provide whatever help you need without any risk.
+		</p>
+	</Accordion>
+
 	<Accordion startOpen summary="Where do the lessons take place?">
 		<p>
 			I can take lessons either at my house, your house, or a libary close by. I live in Doncaster, so Doncaster Library is a good place, but I am flexible.
@@ -24,6 +31,7 @@
 			However, note that if the chosen location requires more than 15 minutes travel, an extra $10 fee will apply, and that travel times may affect my availability as listed on this website.
 		</p>
 	</Accordion>
+
 	<Accordion startOpen summary="Do you have a Working with Children Check?">
 		Yes I do, and can provide its details to you on request.
 	</Accordion>
