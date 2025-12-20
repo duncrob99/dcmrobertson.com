236e2cd9ee7d22abea1d530fed8e6d4cccef05d6
Duncan Robertson <duncan@dcmrobertson.com>
2025-12-20 20:41:46 +1100

    Await cache deletion before returning

--- src/routes/api/cache/purge/+server.ts
+++ src/routes/api/cache/purge/+server.ts
@@ -13,12 +13,21 @@ export const GET = async ({ platform, url }: RequestEvent) => {
 		while (true) {
 			const key_list = await cache?.list({ prefix: `{"label":"${label}` });
 			const keys = key_list?.keys;
+
 			console.log(`Purging ${keys?.length ?? 0} keys for label ${label}`);
-			keys?.forEach(async (key) => {
-				await cache?.delete(key.name);
+			const promises = keys?.map(async (key) => {
+				const val = cache?.delete(key.name);
+				return val;
 			});
 
+			if (promises) {
+				await Promise.all(promises);
+			}
+			console.log(`Finished purging ${keys?.length ?? 0} keys for label ${label}`);
+
 			if (key_list?.list_complete) break;
+
+			console.log(`Waiting to purge next batch of keys for label ${label}`);
 			await new Promise((resolve) => setTimeout(resolve, 500));
 		}
 	}
