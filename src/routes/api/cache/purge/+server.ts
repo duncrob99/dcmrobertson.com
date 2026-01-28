0daee71c920520cda548ca241a2c9fc0aaae3a4d
Duncan Robertson <duncan@dcmrobertson.com>
2026-01-28 22:29:46 +1100

    Retry cache purge if cloudflare rate limit kicks in
    Because they haven't made a proper way to batch delete KVs.

--- src/routes/api/cache/purge/+server.ts
+++ src/routes/api/cache/purge/+server.ts
@@ -1,3 +1,4 @@
+import { ORIGIN } from '$env/static/private';
 import { PlatformWrapper } from '$lib/platform_wrapper';
 import type { RequestEvent } from '@sveltejs/kit';
 
@@ -5,30 +6,40 @@ export const GET = async ({ platform, url }: RequestEvent) => {
 	const platform_wrapper = new PlatformWrapper(platform).platform;
 	const cache = platform_wrapper?.env?.FUNCTION_CACHE;
 
-	const labelsParams = url.searchParams.getAll('label');
+	const labelsParams: string[] = url.searchParams.getAll('label');
 	const labels = labelsParams.length > 0 ? labelsParams : [''];
 	console.log(`Purging cache for labels: ${JSON.stringify(labels)}`);
 
-	for (const label of labels) {
-		while (true) {
-			const key_list = await cache?.list({ prefix: `{"label":"${label}` });
-			const keys = key_list?.keys;
+	try {
+		for (const label of labels) {
+			while (true) {
+				const key_list = await cache?.list({ prefix: `{"label":"${label}` });
+				const keys = key_list?.keys;
 
-			console.log(`Purging ${keys?.length ?? 0} keys for label ${label}`);
-			const promises = keys?.map(async (key) => {
-				const val = cache?.delete(key.name);
-				return val;
-			});
+				console.log(`Purging ${keys?.length ?? 0} keys for label ${label}`);
+				const promises = keys?.map(async (key) => {
+					const val = cache?.delete(key.name);
+					return val;
+				});
 
-			if (promises) {
-				await Promise.all(promises);
+				if (promises) {
+					await Promise.all(promises);
+				}
+				console.log(`Finished purging ${keys?.length ?? 0} keys for label ${label}`);
+
+				if (key_list?.list_complete) break;
+
+				console.log(`Waiting to purge next batch of keys for label ${label}`);
+				await new Promise((resolve) => setTimeout(resolve, 500));
 			}
-			console.log(`Finished purging ${keys?.length ?? 0} keys for label ${label}`);
-
-			if (key_list?.list_complete) break;
-
-			console.log(`Waiting to purge next batch of keys for label ${label}`);
-			await new Promise((resolve) => setTimeout(resolve, 500));
+		}
+	} catch (error) {
+		if (
+			error instanceof Error &&
+			error.message.includes('Too many API requests by single worker invocation.')
+		) {
+			console.log('Rate limit exceeded for single worker invocation');
+			await fetch(`${ORIGIN}/api/cache/purge?label=${labels.join('&label=')}`, { method: 'GET' });
 		}
 	}
 
